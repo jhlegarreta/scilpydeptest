addadcf05e80f92d1cdd89b39e5110e760bbc3b6
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2024-11-13 14:40:23 -0500

    ENH: Initial commit
    Initial commit.

---
+++ scilpydeptest/processing/analysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from scilpydeptest.processing.utils import get_mask
+
+
+def analyze_image(mask_img):
+    return get_mask(mask_img)
---
+++ scilpydeptest/processing/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from scilpy.io.image import get_data_as_mask
+
+
+def get_mask(mask_img):
+    return get_data_as_mask(mask_img)
